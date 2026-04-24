135d9ccbe65148372b3084a2de0e869c4c794567
kengo <pha.kengo.hatsuyama@gmail.com>
2026-04-25 02:08:02 +0900

    add ios capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.flappyguy',
+  appName: 'com.example.flappyguy',
+  webDir: 'dist'
+};
+
+export default config;
